e4a859b568b56fecfb6892d67baa3a34892f70fb
Ricardo <riccamgom@gmail.com>
2023-11-29 01:39:36 +0100

    home seed page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,35 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const simpleWeb = `<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>NestJs Api TEST</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Api Test</h1>
+        <p>Seed the database with test data</p>
+        <button id="fetchButton">SEED</button>
+        <p id="greetingText"></p>
+        <script>
+          document.getElementById('fetchButton').addEventListener('click', () => {
+            fetch('http://localhost:3000/seeder/populate')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('greetingText').textContent = data;
+            })
+            .catch(error => console.error('Error:', error));
+          });
+        </script>
+    </body>
+    </html>`;
+    return simpleWeb;
   }
 }
